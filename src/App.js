c953203bd4a1884aac1d7c8f31c6dcc32174b745
Leonard Neff <leonardneff@icloud.com>
2020-05-01 17:32:08 +0200

    Add keypad and result components, add functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Result from './Result';
+import KeyPad from './KeyPad';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Subtraction Calculator
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state= {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+    
+    if(button === "=") {
+      this.calculate()
+    } else if (button === "C") {
+      this.reset()
+    } else if (button === "CE") {
+      this.backspace()
+    } else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  }
+
+  calculate = () => {
+    try {
+      this.setState({
+        result: "I have discalculia *Link*"
+      })
+    }
+
+    catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState ({
+      result: ""
+    })
+
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Subtraction Calculator
+          </p>
+        </header>
+        <div className="calculator">
+          <Result result={this.state.result} />
+          <KeyPad onClick={this.onClick} />
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
