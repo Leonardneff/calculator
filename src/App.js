71c1971c9a08824436e190c09e35046ead62feff
Leonard Neff <leonardneff@icloud.com>
2020-05-03 19:31:04 +0200

    Code style fixes

--- src/App.js
+++ src/App.js
@@ -4,80 +4,79 @@ import KeyPad from './KeyPad';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state= {
-      result: "",
-      text: false
+        this.state = {
+            result: "",
+            text: false
+        }
     }
-  }
 
-  onClick = button => {
+    onClick = button => {
     
-    if(button === "=") {
-      this.calculate()
-    } else if (button === "C") {
-      this.reset()
-      this.setState({text:false})
-    } else if (button === "CE") {
-      this.backspace()
-      this.setState({text:false})
-    } else if (this.state.text) {
-      this.reset()
-      this.setState({text:false})
-    }
-    else {
-      this.setState({
-        result: this.state.result + button
-      })
-    }
-  }
-
-  calculate = () => {
-    try {
-      this.setState({
-        result: <div className="dycalculia"><span>Sorry, I have</span> <a href="https://en.wikipedia.org/wiki/Dyscalculia" target="_blank" rel="noopener noreferrer">dyscalculia</a></div>,
-        text: true
-      })
+        if (button === "=") {
+            this.calculate()
+        } else if (button === "C") {
+            this.reset()
+            this.setState({text:false})
+        } else if (button === "CE") {
+            this.backspace()
+            this.setState({text:false})
+        } else if (this.state.text) {
+            this.reset()
+            this.setState({text:false})
+        } else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
     }
 
-    catch (e) {
-      this.setState({
-        result: "error"
-      })
+    calculate = () => {
+        try {
+            this.setState({
+                result: <div className="dycalculia"><span>Sorry, I have</span> <a href="https://en.wikipedia.org/wiki/Dyscalculia" target="_blank" rel="noopener noreferrer">dyscalculia</a></div>,
+                text: true
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+        }
+    };
+
+    reset = () => {
+        this.setState ({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        if (this.state.text) {
+            this.setState({
+                result: ""
+            })
+        } else
+            this.setState({
+                result: this.state.result.slice(0, -1)
+            })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                      <h2> Calculating Difference </h2>
+                      <p> Use the numeric keypad to perform a calculation. </p>
+                </header>
+                <div className="calculator">
+                    <Result result={this.state.result} text={this.state.text} />
+                    <KeyPad onClick={this.onClick} />
+                </div>
+            </div>
+        );
     }
-  };
-
-  reset = () => {
-    this.setState ({
-      result: ""
-    })
-
-  };
-
-  backspace = () => {
-    this.setState({
-      result: this.state.result.slice(0, -1)
-    })
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h3>
-            Calculating Difference
-          </h3>
-          <p>Use the numeric keypad to perform a calculation.</p>
-        </header>
-        <div className="calculator">
-          <Result result={this.state.result} text={this.state.text} />
-          <KeyPad onClick={this.onClick} />
-          </div>
-      </div>
-    );
-  }
 }
 
 export default App;
